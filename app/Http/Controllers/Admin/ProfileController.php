b7b27c14697b4fe449c9181ab4fb84199395b00d
OkadaMaho <lumina.a310@gmail.com>
2021-08-26 14:23:25 +0000

    Laravel4kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
-    //
+    
 
 
   public function add()
@@ -15,19 +16,27 @@ class ProfileController extends Controller
     return view('admin.profile.create');
   }
   
-  public function create()
-  {
-    return redirect('admin/profile/create');
-  }
-  
+
   public function edit()
   {
     return view('admin.profile.edit');
   }
   
-  public function update()
+
+ public function create(Request $request)
   {
-    return redirect('admin/profile/edit');
+    
+    $this->validate($request, Profile::$rules);
+    
+    $profile = new Profile;
+    $form =$request->all();
+    
+    unset($form['_token']);
+    
+    $profile->fill($form);
+    $profile->save();
+    
+  
   }
 
 }
