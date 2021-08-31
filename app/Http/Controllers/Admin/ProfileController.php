b50d6a01f3031b79c51ac4e4b129f7069eb1ce98
OkadaMaho <lumina.a310@gmail.com>
2021-08-31 18:35:51 +0000

    Laravel16kadai

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -17,11 +17,6 @@ class ProfileController extends Controller
   }
   
 
-  public function edit()
-  {
-    return view('admin.profile.edit');
-  }
-  
 
  public function create(Request $request)
   {
@@ -29,14 +24,50 @@ class ProfileController extends Controller
     $this->validate($request, Profile::$rules);
     
     $profile = new Profile;
-    $form =$request->all();
+    $form = $request->all();
     
     unset($form['_token']);
     
     $profile->fill($form);
     $profile->save();
     
+    return redirect('admin/profile/create');
+  }
   
+  public function index(Request $request)
+  {
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+      $posts = Profile::where('name', $cond_title)->get();
+      
+    } else {
+      $posts = Profile::all();
+    }
+    return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+  
+  
+  public function edit(Request $request)
+  {
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+        abort(404);
+    }
+    return view('admin.profile.edit', ['profile_form' => $profile]);
+  }
+  
+  
+  public function update(Request $request)
+  {
+    $this->validate($request, Profile::$rules);
+    $profile = Profile::find($request->id);
+    $profile_form = $request->all();
+    
+    unset($profile_form['_token']);
+
+    $profile->fill($profile_form)->save();
+
+    return redirect('admin/profile/');
   }
 
 }
