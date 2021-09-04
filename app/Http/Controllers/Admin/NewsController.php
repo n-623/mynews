344592fd3bf740e2b83da973b5485de650f34244
OkadaMaho <lumina.a310@gmail.com>
2021-09-04 09:38:26 +0000

    heroku02

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\News;
 use App\History;
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -24,8 +25,8 @@ class NewsController extends Controller
     
     // formに画像があれば、保存する
     if (isset($form['image'])) {
-      $path = $request->file('image')->store('public/image');
-      $news->image_path = basename($path);
+      $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+      $news->image_path = Storage::disk('s3')->url($path);
     } else {
       $news->image_path = null;
     }
@@ -76,11 +77,12 @@ class NewsController extends Controller
     if ($request->remove == 'true') {
           $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
-          $path = $request->file('image')->store('public/image');
-          $news_form['image_path'] = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news_form['image_path'] = $news->image_path;
       }
+    
       
       unset($news_form['image']);
       unset($news_form['remove']);
--- resources/views/layouts/profilefront.blade.php
+++ resources/views/layouts/profilefront.blade.php
@@ -12,8 +12,8 @@
      <link rel="dns-prefetch" href="https://fonts.gstatic.com">
      <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
      <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-     <link href="{{ secure_asset('css/profilefront.css') }}" rel="stylesheet">
- </head>
+     <link href="{{ asset('css/profilefront.css') }}" rel="stylesheet">
+ </head>   
  <body>
         <div id="app">
             <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
